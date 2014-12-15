1459d57453dadff3a2058474d8a7d05c82a54b27
tmartin1 <timothy.m.martin@gmail.com>
2014-12-15 12:30:06 -0800

    Finish Taxonomy

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+RetiredForagerBee.prototype.treasureChest = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.gamble = function(){
+  this.treasureChest.push('treasure');
+};
+
+RetiredForagerBee.prototype.forage = function(){
+  return 'I am too old, let me play cards instead';
+};
